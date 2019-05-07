c3a561402ab932b01ba105c9b0d0356cd0a53ec8
Trương Khánh Duy <truongkhanhduy9@gmail.com>
2019-05-07 17:56:30 +0700

    update unit test

--- lexorank.test.ts
+++ lexorank.test.ts
@@ -1,4 +1,3 @@
-
 import {RankUtils} from '../../src/utilities/rank.utilities';
 import {expect} from '@loopback/testlab';
 
@@ -7,6 +6,18 @@ describe('Rank', () => {
   it('Test success empty prev empty next', async () => {
     let [rank, ok] = _rank.insert('', '');
     expect(rank).equal('U');
+    expect(ok).equal(true);
+  });
+
+  it('Test success empty prev', async () => {
+    let [rank, ok] = _rank.insert('', '2');
+    expect(rank).equal('1');
+    expect(ok).equal(true);
+  });
+
+  it('Test success empty next', async () => {
+    let [rank, ok] = _rank.insert('x', '');
+    expect(rank).equal('y');
     expect(ok).equal(true);
   });
 });
