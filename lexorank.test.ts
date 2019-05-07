6f50c8da4b4f11807e2850c67064667a0f28d963
Trương Khánh Duy <truongkhanhduy9@gmail.com>
2019-05-07 17:55:58 +0700

    Add rank unit test

--- lexorank.test.ts
+++ lexorank.test.ts
@@ -1,1 +1,12 @@
 
+import {RankUtils} from '../../src/utilities/rank.utilities';
+import {expect} from '@loopback/testlab';
+
+describe('Rank', () => {
+  let _rank = new RankUtils();
+  it('Test success empty prev empty next', async () => {
+    let [rank, ok] = _rank.insert('', '');
+    expect(rank).equal('U');
+    expect(ok).equal(true);
+  });
+});
